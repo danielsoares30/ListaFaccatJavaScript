844f0688b897d7f6e974ee4bebf8f43d1b7990de
danielsoares30 <149394340+danielsoares30@users.noreply.github.com>
2025-04-09 18:46:33 -0300

    Delete JavaScript Onde directory

--- JavaScript Onde/externo/myscript.js
+++
@@ -1,6 +0,0 @@
-function myFunction() {
-    document.getElementById('demo').innerHTML = 'O Luan se complicou!';
-}
-function myFunction2() {
-    document.getElementById('demo').innerHTML = 'O professor comeu o cerebro do luan';
-}
